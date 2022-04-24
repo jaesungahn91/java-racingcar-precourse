20242954d4eee6ce1ea5c7cfb66d38d6676a965f
jaesungahn91 <jaesungahn91@gmail.com>
2022-04-24 23:32:28 +0900

    feat: 자동차 이름 5자 이하 검증 추가

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -2,13 +2,25 @@ package racingcar.domain;
 
 public class Name {
 
+    private static final int NAME_MIN_LENGTH = 5;
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     private String value;
 
-    public Name(String value) {
-        this.value = value;
+    public Name(String name) {
+        validation(name);
+        this.value = name;
+    }
+
+    private void validation(String name) {
+        if (name.length() > NAME_MIN_LENGTH) {
+            System.out.println(ERROR_MESSAGE);
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getValue() {
         return value;
     }
+
 }
--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,5 +13,12 @@ class NameTest {
     void 이름_생성(final String name) {
         assertThat(new Name(name).getValue()).isEqualTo(name);
     }
-    
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobicronghonux"})
+    void 예외테스트_잘못된_이름_생성(final String name) {
+        Assertions.assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
